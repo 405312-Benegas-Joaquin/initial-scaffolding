0d878128ae0187fb3a9be2bf417286d55a153f1e
Benegas Joaquín Ezequiel <405312@tecnicatura.frc.utn.edu.ar>
2024-09-29 16:33:49 -0300

    update group id

---
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/_example_classes/controllers/DummyController.java
@@ -0,0 +1,43 @@
+package ar.edu.utn.frc.tup.lc.iv._example_classes.controllers;
+
+import ar.edu.utn.frc.tup.lc.iv._example_classes.dtos.ResponseDummyDTO;
+import ar.edu.utn.frc.tup.lc.iv._example_classes.dtos.SaveDummyDTO;
+import ar.edu.utn.frc.tup.lc.iv._example_classes.services.DummyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dummy")
+public class DummyController {
+    @Autowired
+    private DummyService dummyService;
+
+    @GetMapping
+    public ResponseEntity<List<ResponseDummyDTO>> getDummyList() {
+        return ResponseEntity.ok(dummyService.getDummyList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseDummyDTO> getDummyById(@PathVariable Long id) {
+        return ResponseEntity.ok(dummyService.getDummyById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseDummyDTO> createDummy(@RequestBody SaveDummyDTO saveDummyDTO) {
+        return ResponseEntity.ok(dummyService.createDummy(saveDummyDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseDummyDTO> updateDummy(@PathVariable Long id, @RequestBody SaveDummyDTO saveDummyDTO) {
+        return ResponseEntity.ok(dummyService.updateDummy(id, saveDummyDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDummyById(@PathVariable Long id) {
+        dummyService.deleteDummyById(id);
+    }
+}
+  
---
+++ src/test/java/ar/edu/utn/frc/tup/lc/iv/SmokeTest.java
@@ -0,0 +1,33 @@
+package ar.edu.utn.frc.tup.lc.iv;
+
+/*
+
+import ar.edu.utn.frc.tup.lc.iv.services.DummyService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SmokeTest {
+    // Controllers
+    @Autowired
+    private DummyController dummyController;
+
+    // Services
+    @Autowired
+    private DummyService dummyService;
+
+    // Test
+    @Test
+    public void contextLoads() throws Exception {
+        // Controllers
+        assertThat(dummyController).isNotNull();
+
+        // Services
+        assertThat(dummyService).isNotNull();
+    }
+}
+
+ */
